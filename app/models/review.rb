9eca0984b4cb353eb90661a82cbd7309b06e3d20
Sari Vos <vos.sari@gmail.com>
2023-01-18 21:30:32 +0100

    first version done

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, in: 0..5 }
+  validates :rating, inclusion: 0..5
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "Amsterdam", category: "italian" }
+klaproos = { name: "Klaproos", address: "Rotterdam", category: "italian" }
+diwali_palace = { name: "Diwali Palace", address: "Amsterdam", category: "japanese" }
+pizza_west = { name: "Pizza West", address: "56A Shoreditch High St, London E1 6PQ", category: "french" }
+
+[dishoom, pizza_east, klaproos, diwali_palace, pizza_west].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
